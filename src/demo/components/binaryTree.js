7ce649fc98031ce98ca02b1c5dad91e1a9b9db7e
danieljoonlee <danieljoonlee@gmail.com>
2016-05-23 10:15:58 -0700

    disabled binary tree and added quick fix for prop changes

--- src/demo/components/binaryTree.js
+++ src/demo/components/binaryTree.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import createNode from './../d3-examples/binarytree';
-import rd3 from 'react-d3-library';
+// import React, { Component } from 'react';
+// import createNode from './../d3-examples/binarytree';
+// import rd3 from 'react-d3-library';
 
-const RD3Component = rd3.Component
+// const RD3Component = rd3.Component
 
-module.exports = React.createClass({
+// module.exports = React.createClass({
 
-  getInitialState: function() {
-    return {d3: ''}
-  },
+//   getInitialState: function() {
+//     return {d3: ''}
+//   },
 
-  componentDidMount: function() {
-    var node = createNode();
-    this.setState({d3: node});
-  },
+//   componentDidMount: function() {
+//     var node = createNode();
+//     this.setState({d3: node});
+//   },
 
-  regenerate: function() {
-    var node = createNode();
-    this.setState({d3: node});
-  },
+//   regenerate: function() {
+//     var node = createNode();
+//     this.setState({d3: node});
+//   },
 
-  render: function() {
-    return (
-      <div>
-        <RD3Component data={this.state.d3} />
-        <button onClick={this.regenerate}>Regenerate</button>
-      </div>
-    )
-  }
-});
+//   render: function() {
+//     return (
+//       <div>
+//         <RD3Component data={this.state.d3} />
+//         <button onClick={this.regenerate}>Regenerate</button>
+//       </div>
+//     )
+//   }
+// });
